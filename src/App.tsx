5f959a7c98492ad03a618ce6c039801f02cab754
MIRZA YOOSHA MINHAJ <143473452+YooshaMirza@users.noreply.github.com>
2025-04-20 21:14:14 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Routes, Route, Navigate, HashRouter } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import { useAuthStore } from './store/authStore';
 
 // Pages
@@ -21,7 +21,7 @@ function ProtectedRoute({ children }: { children: React.ReactNode }) {
   return <>{children}</>;
 }
 
-function AppRoutes() {
+function App() {
   return (
     <Routes>
       {/* Public routes */}
@@ -58,14 +58,6 @@ function AppRoutes() {
       {/* Fallback route */}
       <Route path="*" element={<Navigate to="/" replace />} />
     </Routes>
-  );
-}
-
-function App() {
-  return (
-    <HashRouter>
-      <AppRoutes />
-    </HashRouter>
   );
 }
 
